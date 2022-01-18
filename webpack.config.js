c0244f3573d1bd861bae3cc7315d08d8ab9bca5a
carne8 <63708866+carne8@users.noreply.github.com>
2022-01-18 18:57:53 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,35 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const Dotenv = require('dotenv-webpack')
+const Dotenv = require("dotenv-webpack")
 
-// export the configuration as an object
+const isProduction = !process.argv.find(v => v.indexOf("webpack-dev-server") !== -1)
+
 module.exports = {
-  mode: "development",
+  mode: isProduction ? "production" : "development",
+  devtool: isProduction ? "source-map" : "eval-source-map",
   entry: "./src/Main.fs.js",
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "out/"),
+    path: path.resolve(__dirname, "output/"),
+    filename: isProduction ? "[name].[contenthash].js" : "[name].js",
+    publicPath: "auto"
+  },
+  devServer: {
+    port: 8080,
+    proxy: undefined,
+    host: "0.0.0.0",
+    hot: true,
+    historyApiFallback: {
+      rewrites: [
+        { from: /./, to: "/index.html" }
+      ]
+    }
   },
   plugins: [
     new Dotenv({
       systemvars: true
     }),
     new HtmlWebpackPlugin({
-      template: "public/index.html",
+      template: "./src/index.html",
     }),
   ],
   module: {
@@ -24,22 +38,21 @@ module.exports = {
         test: /\.(sass|scss|css)$/,
         use: [
           "style-loader",
-          {
-            loader: "css-loader",
-          },
+          "css-loader",
+          "resolve-url-loader",
           {
             loader: "sass-loader",
-            options: { implementation: require("sass") }
+            options: {
+              sourceMap: true,
+              implementation: require("sass")
+            }
           }
-        ],
+        ]
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*)?$/,
-        use: ["file-loader"]
+        type: 'asset/resource'
       }
     ],
-  },
-  resolve: {
-    extensions: [".js"],
   }
 }
